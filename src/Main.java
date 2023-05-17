d8cba4170f97eceb2f704f92d588f1baf4f95404
Bychkova Mariya <bychk-mariya@yandex.ru>
2023-05-17 14:50:13 +0300

    Bonus_system

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int balans = 90; // текущий баланс
+        int x = 1000; // сумма пополнения
+        if (x >= 1000) {
+            System.out.println("Ваш баланс: ");
+            System.out.println((x / 100) + (balans) + (x) + " рублей.");
+            System.out.println("Начисленно " +(x / 100)+" баллов.");
+        } else {
+            System.out.println("Ваш баланс:");
+            System.out.println((x) + (balans) + " рублей");
+        }
+        System.out.println("Хорошего дня!");
     }
 }
